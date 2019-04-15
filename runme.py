3e5cfb081bcae6ca49c5417d3d2f044da4b41179
liuzhao <liuzhao@us.ibm.com>
2019-04-15 15:57:43 -0500

    first merge

---
+++ runme.py
@@ -0,0 +1,10 @@
+CEND      = '\33[0m'
+CBOLD     = '\33[1m'
+CBLINK    = '\33[5m'
+CRED    = '\33[31m'
+CGREEN  = '\33[32m'
+CYELLOW = '\33[33m'
+CREDBG    = '\33[41m'
+CBLUEBG   = '\33[44m'
+
+print 'You are so cute, ' + CBLINK + CYELLOW + 'Mihu' + CEND + ' !!!'
